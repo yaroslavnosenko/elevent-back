ef938916ffa76f35ce991c581a4d9afbc026c070
Denis Panov <deonis.panoff@gmail.com>
2019-03-30 19:14:41 +0100

    db test

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var connection = mysql.createConnection({
+    host: '35.246.154.124',
+    user: 'root',
+    password: 'w2a6c8wvk',
+    database: 'elevent'
 });
 
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
